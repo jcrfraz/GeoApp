46af7c8dff802516027b611d0c121406b0313b60
Juan Carlos Rodriguez Fraz <11470207@itcampeche.edu.mx>
2016-02-24 22:51:34 -0600

    Version con demostración de manejo de eventos del proyecto GeoApp

--- app/src/main/java/com/example/juan/geoapp/GeoActivity.java
+++ app/src/main/java/com/example/juan/geoapp/GeoActivity.java
@@ -2,12 +2,38 @@ package com.example.juan.geoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GeoActivity extends AppCompatActivity {
+    private Button mBotonCierto;
+    private Button mBotonFalso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+
+        //viste nicte ya quedo listo solo eso copia lo este codigo
+
+        mBotonCierto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_correcto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        mBotonFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_incorrecto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+
+
     }
 }
